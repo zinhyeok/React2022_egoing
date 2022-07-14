365f42bf68f05c7a651ad86fc9892d0fd8c408ac
zinhyeok <david9pjh@gmail.com>
2022-07-14 21:38:14 +0900

    Feat: [props 5강]

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(){
+function Header(props){
+  console.log(props, props.title);
   return (
     <header>
-      <h1><a href="/">React</a></h1>
+      <h1><a href="/">{props.title}</a></h1>
     </header>
   )
 }
 
-function Nav(){
+function Nav(props){
+//props.topic, props중 어떤걸 가져올지 꼭 명시해줘야한다
+const lis =  props.topics.map((prop) => <li key={prop.id}><a href={'/read/'+prop.id}>{prop.title}</a></li>);
+  
   return(
   <nav>
       <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-
+        {lis}
       </ol>
   </nav>
   )
 }
 
-function Article(){
+function Article(props){
   return(
   <article>
-    <h2>Welcome</h2>
-    Hello, React!
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
   )
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javaScript', body:'javaScript is ...'}
+  ]
   return (
    <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello React"></Article>
    </div>
   );
 }
