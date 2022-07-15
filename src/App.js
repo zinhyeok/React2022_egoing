d3c549cbbb6ca916d2a5bc754db9940298443c2a
zinhyeok <david9pjh@gmail.com>
2022-07-15 15:11:02 +0900

    Feat:[8,9강 create, update]

--- src/App.js
+++ src/App.js
@@ -43,19 +43,65 @@ function Article(props){
   )
 }
 
+function Create(props){
+  return(
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type='text' name='title' placeholder='title' /></p>
+        <p><textarea name='body' placeholder='body'></textarea></p>
+        <p><input type='submit' value="Create"></input></p>
+      </form>
+    </article>
+  )
+}
+
+function Update(props){
+  //props는 외부에서 사용하는 상태입으로 state로 내부에서 변경가능하게 바꿔준다
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return(
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+        <p><input type='text' name='title' placeholder='title' value={title} onChange={e=>{
+          setTitle(e.target.value);
+        }}/></p>
+        <p><textarea name='body' placeholder='body' value={body} onChange={e=>{
+          setBody(e.target.value);
+        }}></textarea></p>
+        <p><input type='submit' value="Update"></input></p>
+      </form>
+    </article>
+  )
+}
+
 function App() {
 //  const mode = useState('Welcome');
   //state는 배열 [0] 초기값, [1]은 값을 바꿀때 사용
   const [mode, setMode] = useState('Welcome');
   const [id, setId] = useState('null')
-  
-  const topics = [
+  //useState() state는 기본적으로 primitive type
+  // reference type이면? {...value}로 복제해야함 
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javaScript', body:'javaScript is ...'}
-  ]
+  ])
 
   let content = null;
+  let contextControl = null;
+
   if(mode === 'Welcome'){
     content = <Article title="Welcome" body="Hello React"></Article>
   }
@@ -66,15 +112,61 @@ function App() {
     if(topics[i].id === Number(id)){
       title = topics[i].title;
       body= topics[i].body; 
-    }
-  }
+    }}
     content = <Article title={title} body={body}></Article>
+    contextControl = <li><a href={"/update/"+id} onClick={event =>
+    { event.preventDefault();
+      setMode('Update');
+    }}
+    >Update</a></li>
+  }
+  else if(mode === 'Create'){
+    content= <Create onCreate={(title,body)=>{
+      const newTopic = {id:topics.length +1, title: title, body:body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      //state의 primitive type 특성
+      setTopics(newTopics);
+      //작성한 글로 이동
+      setMode('Read');
+      setId(newTopics.length);
+    }}></Create>
+  }
+  else if(mode==='Update'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+    if(topics[i].id === Number(id)){
+      title = topics[i].title;
+      body= topics[i].body; 
+    }}
+    content=<Update title={title} body={body} onUpdate={(title,body)=>{
+        console.log(title,body);
+        const newTopics = [...topics];
+        const updatedTopic = {id:id, title: title, body: body};
+        for(let i=0; i<topics.length; i++){
+          if(newTopics[i].id === id){
+            newTopics[i] = updatedTopic;
+            break; 
+          }}
+          setTopics(newTopics);
+          setMode('Read');
+          
+    }}></Update>
   }
   return (
    <div>
       <Header title="REACT" onChangeMode={()=> {setMode('Welcome');}}></Header>
       <Nav topics={topics} onChangeMode={(id)=> {setMode('Read'); setId(id);}}></Nav>
       {content}
+      <ul>
+        <li>
+          <a href="/create" onClick={event => {
+            event.preventDefault();
+            setMode('Create');
+          }}>Create</a>
+        </li>
+          {contextControl}
+      </ul>
    </div>
   );
 }
